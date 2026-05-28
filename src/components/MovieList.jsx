c75a97b1a183ecafef7b10c40057042b6845f892
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-05-28 10:43:55 +0200

    refactor: extract movie details to a separate component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 
+import MovieSummary from "./MovieSummary"
+
 import movies from "../data/movies.json"
 
 
@@ -32,17 +34,11 @@ function MovieList() {
 
             {moviesToDisplay.map((movieObj, i, arr) => {
                 return (
-                    <div key={movieObj.id} className="card">
-                        <h3>{movieObj.title}</h3>
-
-                        {movieObj.imgURL
-                            && <img src={movieObj.imgURL} alt="Movie poster" />}
-
-                        <p>Year: {movieObj.year}</p>
-                        <p>Rating: {movieObj.rating}</p>
-
-                        <button onClick={() => { deleteMovie(movieObj.id) }}>Delete</button>
-                    </div>
+                    <MovieSummary 
+                        key={movieObj.id}
+                        movieDetails={movieObj}
+                        onDelete={deleteMovie}
+                    />
                 )
             })}
         </>
---
+++ src/components/MovieSummary.jsx
@@ -0,0 +1,18 @@
+
+function MovieSummary(props){
+    return (
+        <section className="card">
+            <h3>{props.movieDetails.title}</h3>
+
+            {props.movieDetails.imgURL
+                && <img src={props.movieDetails.imgURL} alt="Movie poster" />}
+
+            <p>Year: {props.movieDetails.year}</p>
+            <p>Rating: {props.movieDetails.rating}</p>
+
+            <button onClick={() => { props.onDelete(props.movieDetails.id) }}>Delete</button>
+        </section>
+    )
+}
+
+export default MovieSummary
