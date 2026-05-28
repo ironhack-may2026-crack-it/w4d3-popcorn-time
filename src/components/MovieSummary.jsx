209c9974951129e3e8e3895043ef8105f24fd6c5
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-05-28 15:08:48 +0200

    feat: add movieDetails page (routing + url params)

--- src/components/MovieSummary.jsx
+++ src/components/MovieSummary.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 function MovieSummary(props) {
     return (
@@ -7,10 +8,13 @@ function MovieSummary(props) {
             {props.movieDetails.imgURL
                 && <img src={props.movieDetails.imgURL} alt="Movie poster" />}
 
-            <p>Year: {props.movieDetails.year}</p>
             <p>Rating: {props.movieDetails.rating}</p>
 
             <button onClick={() => { props.onDelete(props.movieDetails.id) }}>Delete</button>
+
+            <Link to={`/movies/${props.movieDetails.id}`}>
+                <button>More details</button>
+            </Link>
         </section>
     )
 }
